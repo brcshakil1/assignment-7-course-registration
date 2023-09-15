4efc73bf842d09a6cb6556d93004361051825adf
brcshakil1 <brcshakil72@gmail.com>
2023-09-15 12:11:21 +0600

    map through in courses and called Card component and sent course as a props inside of Card Component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -12,7 +12,11 @@ const Cards = () => {
     return (
         <div className='md:w-2/3'>
             <h1 className='text-4xl font-bold text-center'>Course Registration</h1>
-            <Card />
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    courses.map(course => <Card key={course.id} course={course}/>)
+                }
+            </div>
         </div>
     );
 };
